a56e7b40e67ac63491b2aa3b72f52763da5efeea
e601201 <k.arthur1111@gmail.com>
2022-07-06 19:58:17 +0900

    "[add]lessons"

---
+++ js_lesson/lesson_1/index.js
@@ -0,0 +1,31 @@
+let huga = "hello world";
+const inoki = ['イーチ','ニー','サン','だー']
+
+// let index = 0;
+// while(index < inoki.length ){
+//   console.log(inoki[index])
+//   index ++;
+// }
+
+const test = (arg) => {
+  if (inoki.length > arg) {
+    console.log('ぼんばいえ');
+  } else {
+    console.log('ボンバー');
+  }
+};
+
+const car = {
+  color: 'white',
+  size: 'big',
+  price: 500 ,
+  sound: () => {
+    console.log("ブルルーン");
+  }
+};
+
+const collectButton = document.getElementsByTagName('button')[0]
+
+collectButton.addEventListener('click', ()=>{
+  console.log(car.color)
+});
